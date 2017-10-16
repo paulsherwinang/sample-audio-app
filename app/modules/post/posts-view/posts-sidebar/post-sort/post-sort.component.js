c036dd293e625cc10b43a01ee3c1b711300b4c00
Paul Sherwin Ang <paul.sherwin.ang@gmail.com>
2017-10-16 22:04:49 +0800

    fixed template issue

--- app/modules/post/posts-view/posts-sidebar/post-sort/post-sort.component.js
+++ app/modules/post/posts-view/posts-sidebar/post-sort/post-sort.component.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp.post-sort.component', [])
     .component('postSort', {
-        templateUrl: 'modules/post/posts-sidebar/post-sort/post-sort.component.html',
+        templateUrl: 'modules/post/posts-view/posts-sidebar/post-sort/post-sort.component.html',
         bindings: {
             onSelectChange: '&',
             sortValue: '<'
--- app/modules/post/posts-view/posts-sidebar/posts-sidebar.component.js
+++ app/modules/post/posts-view/posts-sidebar/posts-sidebar.component.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('myApp.posts-sidebar.component', [])
+angular.module('myApp.posts-sidebar.component', [
+    'myApp.post-sort.component',
+    'myApp.post-group.component',
+])
 .component('postsSidebar', {
     templateUrl: 'modules/post/posts-view/posts-sidebar/posts-sidebar.component.html',
     bindings: {
